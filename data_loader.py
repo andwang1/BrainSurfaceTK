9940d78c4dec386ee89111b6e0fa4812c4d61256
vv19 <vv19@ic.ac.uk>
2020-02-04 21:25:17 +0000

    Loader reads vtk files.

--- data_loader.py
+++ data_loader.py
@@ -1,135 +1,173 @@
 import os
 import os.path as osp
-import shutil
-import glob
-from deepl_brain_surfaces.read_meta import read_meta
+
+import numpy as np
+import vtk
+from vtk.util.numpy_support import vtk_to_numpy
+
+from read_meta import read_meta
 import torch
-from torch_geometric.data import InMemoryDataset, download_url, extract_zip
+from torch_geometric.data import InMemoryDataset
 from torch_geometric.io import read_off
+from torch_geometric.data import DataLoader
+from torch_geometric.data import Data
+import torch_geometric.transforms as T
+
 
 class MyOwnDataset(InMemoryDataset):
-    def __init__(self, root, train=True, transform=None, pre_transform=None):
-        super(MyOwnDataset, self).__init__(root, transform, pre_transform)
-        # self.pt_path = '/vol/biomedic2/aa16914/shared/MScAI_brain_surface/data/processed_off/'
+    def __init__(self, root, label_class='gender', train=True, transform=None,
+                        pre_transform=None, pre_filter=None, data_folder=None):
+
+        self.train = train
+        self.categories = {'gender': 2, 'birth_age': 3, 'weight': 4, 'scan_age': 6, 'scan_num': 7}
+        self.meta_column_idx = self.categories[label_class]
+        self.classes = dict()
+
+        if data_folder is None:
+            self.data_folder = '/vol/biomedic2/aa16914/shared/MScAI_brain_surface/data/'
+        else:
+            self.data_folder = data_folder
+
+        super(MyOwnDataset, self).__init__(root, transform, pre_transform, pre_filter)
 
         path = self.processed_paths[0] if train else self.processed_paths[1]
         self.data, self.slices = torch.load(path)
 
-        print('Processed paths: ', self.processed_paths)
-
-        self.categories = ['gender', 'scan_age']
-        # self.data, self.slices = torch.load(self.processed_paths[0])
-
     @property
     def raw_file_names(self):
         '''A list of files in the raw_dir which needs to be found in order to skip the download.'''
-        return [
-            'bathtub', 'bed', 'chair', 'desk', 'dresser', 'monitor',
-            'night_stand', 'sofa', 'table', 'toilet'
-        ]
+        return []
 
     @property
     def processed_file_names(self):
         '''A list of files in the processed_dir which needs to be found in order to skip the processing.'''
-        return ['training.pt', 'test.pt']
+        return ['training.pt', 'test.pt', 'a'] # TODO: REMOVE 'a' WHEN DONE DEBUGGING
 
     def download(self):
-        # Download to `self.raw_dir`.
+        '''No need to download data.'''
         pass
 
     def process(self):
         '''Processes raw data and saves it into the processed_dir.'''
         # Read data into huge `Data` list.
-        print('PROCESSING')
-        print(self.processed_paths[0])
-        torch.save(self.process_set('train'), self.processed_paths[0])
-        # torch.save(self.process_set('test'), '/vol/biomedic2/aa16914/shared/MScAI_brain_surface/data/processed_off')
+        if self.train:
+            torch.save(self.process_set(), self.processed_paths[0])
+        else:
+            torch.save(self.process_set(), self.processed_paths[1])
 
-
-    def process_set(self, dataset):
-
-        # 0. Get column number of meta data corresponding to 'sex' (or other categories)
-        # meta_column_idx = self.categories.index('gender')
-        meta_column_idx = 2
+    def process_set(self):
 
         # 0. Get meta data
         meta_data = read_meta()
 
         # 0. Get patient id number and label columns (0 = patient id, meta_column_idx = label column (eg. sex))
-        meta_data = meta_data[:, [0, meta_column_idx]]
+        if self.train: # TODO: MAKE SPLIT CORRECTLY (ACCORDING TO INPUT IN PERCENTAGE)
+            meta_data = meta_data[:, [0, self.meta_column_idx]]
+        else:
+            meta_data = meta_data[:, [0, self.meta_column_idx]]
 
         # 1. Initialise the variables
-        folder = '/vol/biomedic2/aa16914/shared/MScAI_brain_surface/data/'
         data_list = []
         categories = set(meta_data[:, 1])           # Set of categories {male, female}
 
         # 2. Create category dictionary (mapping: category --> class), e.g. 'male' --> 0, 'female' --> 1
-        classes = dict()
         for class_num, category in enumerate(categories):
-            classes[category] = class_num
+            self.classes[category] = class_num
 
         # 3. Iterate through all patient ids
         for idx, patient_id in enumerate(meta_data[:, 0]):
+            # TODO: GET ALL THE VTK FILES IN ONE PLACE.
+            # TODO: PREDEFINE THESE IN __INIT__
+            repo = "/vol/biomedic2/aa16914/shared/MScAI_brain_surface/data/sub-" + patient_id + "/ses-38200/anat/vtk/"
+            file_name = "sub-" + patient_id + "_ses-38200_hemi-L_space-dHCPavg32k_inflated_drawem_thickness_thickness_curvature_sulc_myelinmap_myelinmap.vtk"
 
-            # Create path to file
-            path = folder + patient_id + '_L_pial' +'.off'
+            file_path = repo + file_name
 
-            # Try read patient data
-            if os.path.isfile(path):
-                # print('Reading patient: {}'.format(patient_id))
-                data = read_off(path)
-                data.y = torch.tensor([classes[meta_data[:, 1][idx]]])   # classes[meta_data[:, 1][idx]] returns class_num from classes using key (e.g. 'female' -> 1)
+            if os.path.isfile(file_path):
+                reader = vtk.vtkPolyDataReader()
+                reader.SetFileName(file_path)
+                reader.Update()
 
-                # print(classes[meta_data[:, 1][idx]])
-                # print('='*30)
-                # print('='*30)
-                # print('Patient ', patient_id)
-                # print(data)
-                # print(data.y)
+                # Points
+                points = torch.tensor(np.array(reader.GetOutput().GetPoints().GetData()))
 
+
+                # Getting the faces # TODO: NEED TO CHECK (COMPARE FACES WITH WHAT TORCH GIVES)
+                cells = reader.GetOutput().GetPolys()
+                nCells = cells.GetNumberOfCells()
+                array = cells.GetData()
+                nCols = array.GetNumberOfValues() // nCells
+                numpy_cells = vtk_to_numpy(array)
+                numpy_cells = numpy_cells.reshape((-1, nCols))
+                faces = torch.tensor(numpy_cells[:, [1, 2, 3]].transpose())
+
+
+                # Features # TODO: ADD ALL THE FEATURES THAT ARE NEEDED.
+                # array_drawem = vtk_to_numpy(reader.GetOutput().GetPointData().GetArray(0))
+                use_thinckness = True
+                if use_thinckness:
+                    array_corrThickness = vtk_to_numpy(reader.GetOutput().GetPointData().GetArray(1))
+                    x = torch.tensor(array_corrThickness)
+                # array_2 = vtk_to_numpy(reader.GetOutput().GetPointData().GetArray(2))  # TODO: ASK ABOUT THIS
+                # array_Curvature = vtk_to_numpy(reader.GetOutput().GetPointData().GetArray(3))
+                # array_Sulc = vtk_to_numpy(reader.GetOutput().GetPointData().GetArray(4))
+                # array_SmoothedMyelinMap = vtk_to_numpy(reader.GetOutput().GetPointData().GetArray(5))
+                # array_MyelinMap = vtk_to_numpy(reader.GetOutput().GetPointData().GetArray(6))
+
+                # classes[meta_data[:, 1][idx]] returns class_num from classes using key (e.g. 'female' -> 1)
+                y = torch.tensor([self.classes[meta_data[:, 1][idx]]])
+
+                data = Data(x=x, pos=points, y=y, face=faces)
                 data_list.append(data)
             else:
                 continue
 
+            ##  KEEPING FOR NOW
+            # # Create path to file. _L_pial for now.
+            # path = self.data_folder + patient_id + '_L_pial' +'.off'
+            #
+            # # Try read patient data
+            # if os.path.isfile(path):
+            #     data = read_off(path)
+            #     data.y = torch.tensor([self.classes[meta_data[:, 1][idx]]])   # classes[meta_data[:, 1][idx]] returns class_num from classes using key (e.g. 'female' -> 1)
+            #
+            #     data_list.append(data)
+            # else:
+            #     continue
 
         # # 4. Attempt any pre-processing that is required
-        # if self.pre_filter is not None:
-        #     data_list = [d for d in data_list if self.pre_filter(d)]
-        #
-        # if self.pre_transform is not None:
-        #     data_list = [self.pre_transform(d) for d in data_list]
+        if self.pre_filter is not None:
+            data_list = [d for d in data_list if self.pre_filter(d)]
 
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(d) for d in data_list]
 
-        data, slices = self.collate(data_list)
-        print(data, '\n\n\n', slices)
-
+        print(len(data_list))
         return self.collate(data_list)
 
-        # categories = glob.glob(osp.join(self.raw_dir, '*', ''))
-        # print('Raw directory is ', self.raw_dir)
-        # categories = sorted([x.split(os.sep)[-2] for x in categories])
-        # print('Our categories are ', categories)
-        #
-        # data_list = []
-        # for target, category in enumerate(categories):
-        #     folder = osp.join(self.raw_dir, category, dataset)                  # raw directory + 'bathtub' + 'train' or 'test'
-        #     paths = glob.glob('{}/{}_*.off'.format(folder, category))           # PATHS = a list of paths to files /bathtub_0001.off, etc.
-        #     for path in paths:
-        #         data = read_off(path)                                           # data.pos contains all the POINTS (point cloud)
-        #         data.y = torch.tensor([target])                                 # data.y contains all the LABELS
-        #         data_list.append(data)
-        #
-        # if self.pre_filter is not None:
-        #     data_list = [d for d in data_list if self.pre_filter(d)]
-        #
-        # if self.pre_transform is not None:
-        #     data_list = [self.pre_transform(d) for d in data_list]
-        #
-        # return self.collate(data_list)
 
 if __name__ == '__main__':
-    path = osp.join(
-        osp.dirname(osp.realpath(__file__)), '..', 'data/ModelNet10')
+    # Path to where the data will be saved.
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data')
+    # Transformations, scaling and sampling 102 points (doesn't sample faces).
+    pre_transform, transform = T.NormalizeScale(), T.SamplePoints(1024)
 
-    myDataset = MyOwnDataset(path)
-    myDataset.process()
+    myDataset = MyOwnDataset(path, train=True, transform=transform, pre_transform=pre_transform)
+    train_loader = DataLoader(myDataset, batch_size=1, shuffle=False)
+
+    print(list(train_loader))
+
+    # for i, (batch, pos, y) in enumerate(train_loader):
+    #     print(batch)
+    #     print(pos)
+    #     print(y)
+    #     print('_____________')
+
+     # Printing dataset without sampling points. Will include faces.
+    # for i, (batch, face, pos, x, y) in enumerate(train_loader):
+    #     print(batch)
+    #     print(face)
+    #     print(pos)
+    #     print(x)
+    #     print(y)
+    #     print('_____________')
