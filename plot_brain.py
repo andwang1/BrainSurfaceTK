5c51615568b7bc386b3dd430c18b5832941f7f9e
az519 <alexey.zakharov19@imperial.ac.uk>
2020-03-26 16:57:36 +0100

    Working from home - transfering files
    Former-commit-id: 1807b80d8e5e38e32d55ce1c5925a6ea66a408b8

--- plot_brain.py
+++ plot_brain.py
@@ -4,7 +4,7 @@ from pyvista_examples import plot
 
 if __name__ == '__main__':
 
-    with open('./2/data_validation1.pkl', 'rb') as file:
+    with open('./5/data_validation1.pkl', 'rb') as file:
         data, labels, pred = pickle.load(file)
 
     plot(data, labels, pred)
