123d423f9e19cf742139a97e9dc0dc8b51bdc575
andyw <andywang1194@gmail.com>
2020-03-26 22:39:11 +0000

    adding indices for 50
    Former-commit-id: 8d22f8480a7fa133acb691f2fd3ebafa2e155c4e

--- MeshCNN-master/andy_code/create_folder_struct_for_regression.py
+++ MeshCNN-master/andy_code/create_folder_struct_for_regression.py
@@ -4,7 +4,7 @@ import pandas as pd
 from shutil import copyfile
 
 # Where them brains at
-source_dir = r"/vol/project/2019/545/g1954504/Andy/deepl_brain_surfaces/MeshCNN-master/datasets/all_brains"
+source_dir = r"/vol/project/2019/545/g1954504/Andy/deepl_brain_surfaces/MeshCNN-master/datasets/all_brains_50"
 # Where them brains should be at
 target_dir = r"/vol/project/2019/545/g1954504/Andy/deepl_brain_surfaces/MeshCNN-master/datasets/brains_reg_red50"
 
