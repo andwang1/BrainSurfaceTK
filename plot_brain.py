f2ce92304399766d4914fb01511550506757f834
az519 <alexey.zakharov19@imperial.ac.uk>
2020-03-26 17:19:06 +0100

    SSHing
    Former-commit-id: 6e512b16fab8e5b2dc5478b0d9a70bf88a764b7e

--- plot_brain.py
+++ plot_brain.py
@@ -4,7 +4,7 @@ from pyvista_examples import plot
 
 if __name__ == '__main__':
 
-    with open('./5/data_validation1.pkl', 'rb') as file:
+    with open('./6/data_validation2.pkl', 'rb') as file:
         data, labels, pred = pickle.load(file)
 
     plot(data, labels, pred)
