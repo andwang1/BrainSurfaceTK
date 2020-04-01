4fd8385a2fe8a1aa807a7c911678114be8d5733a
andyw <andywang1194@gmail.com>
2020-04-01 19:41:54 +0100

    fixing code to make compatible with both halves
    Former-commit-id: 75fb6db918444f1be1f7d1137d109d2965c717b3

--- MeshCNN-master/scripts/brains/train_exp_red50_24gb_pool.sh
+++ MeshCNN-master/scripts/brains/train_exp_red50_24gb_pool.sh
@@ -3,7 +3,7 @@
 ## run the training
 python3 train.py \
 --dataroot datasets/brains_reg_red50 \
---checkpoints_dir checkpoints/red50_run2 \
+--checkpoints_dir checkpoints/red50_24gb_pool \
 --export_folder checkpoints/mesh_collapses \
 --name brains \
 --ninput_edges 48735 \
