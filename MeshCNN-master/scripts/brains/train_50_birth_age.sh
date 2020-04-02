91cd5ec9efa1959667f6f3e41498a6bf353de8e1
Francis Ward <frw19@vm-shell1.doc.ic.ac.uk>
2020-04-02 11:05:10 +0100

    birth age regression
    Former-commit-id: b28f26ac7efffa5782d6348eba26c8dfaf962289

--- MeshCNN-master/scripts/brains/train_50_birth_age.sh
+++ MeshCNN-master/scripts/brains/train_50_birth_age.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
-
- ## run the training
- python3 train.py \
+## run the training
+python3 train.py \
  --dataroot datasets/brains_reg_red50 \
- --checkpoints_dir checkpoints/red50_run2 \
+ --checkpoints_dir checkpoints/red50_birth_age1 \
  --export_folder checkpoints/mesh_collapses \
  --name brains \
  --epoch_count 1 \
- --niter 5 \
- --batch_size 1 \
- --ncf 64 112 128 \
- --pool_res 3000 2500 2000 \
  --norm group \
- --resblocks 1 \
- --flip_edges 0.2 \
- --slide_verts 0.2 \
  --num_aug 1 \
  --verbose_plot \
+ --print_freq 10 \
+ --seed 0 \
  --dataset_mode regression \
  --label birth_age \
- --print_freq 14 \
+ --niter 5 \
+ --niter_decay 100 \
+ --batch_size 1 \
+ --ncf 64 112 128 \
+ --pool_res 4000 3500 3000 \
  --ninput_edges 9000 \
-
+ --verbose_plot \
