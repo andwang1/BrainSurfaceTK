67fd0e75a06194b617f72baf531c225f9c751590
aw1912 <aw1912@ic.ac.uk>
2020-04-02 10:25:49 +0100

    adding new scripts
    Former-commit-id: e1984c12782f4bf87ace9b6f93a35f3d3141e78d

--- MeshCNN-master/scripts/brains/train_exp_red90_12gb_ep50.sh
+++ MeshCNN-master/scripts/brains/train_exp_red90_12gb_ep50.sh
@@ -2,11 +2,11 @@
 
 ## run the training
 python3 train.py \
---dataroot datasets/brains_reg_red50 \
---checkpoints_dir checkpoints/red50_24gb_conv \
+--dataroot datasets/brains_reg_red90 \
+--checkpoints_dir checkpoints/red90_12gb_ep50 \
 --export_folder checkpoints/mesh_collapses \
 --name brains \
---ninput_edges 48735 \
+--ninput_edges 9000 \
 --epoch_count 1 \
 --norm group \
 --num_aug 1 \
@@ -14,8 +14,8 @@ python3 train.py \
 --print_freq 10 \
 --seed 0 \
 --dataset_mode regression \
---niter 5 \
---niter_decay 100 \
---batch_size 1 \
---ncf 512 1024 2048 2048  \
---pool_res 10000 8000 6000 4000 \
+--niter 1 \
+--niter_decay 50 \
+--batch_size 14 \
+--ncf 128 128 256 256 \
+--pool_res 6000 5000 4000 3000 \
