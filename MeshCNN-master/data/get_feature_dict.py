f08765bf8e6dbaa839526e46514b6c245549f81f
Andy Wang <aw1912@ic.ac.uk>
2020-04-09 21:57:54 +0100

    changing path of meta data
    Former-commit-id: f281fd57dfc18a4753a4db3f844b853f4d3d447d

--- MeshCNN-master/data/get_feature_dict.py
+++ MeshCNN-master/data/get_feature_dict.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 meta_data_path = "/vol/biomedic2/aa16914/shared/MScAI_brain_surface/andy/meta_data_rewritten.tsv"
+meta_data_path = "MeshCNN-master/util/meta_data.tsv"
 meta_data = pd.read_csv(meta_data_path, delimiter="\t")
 
 
