c138e4d340ee29fd59165285e30b588a62c809ee
Andy Wang <aw1912@ic.ac.uk>
2020-04-13 15:32:19 +0100

    changing dropout position in architecture
    Former-commit-id: b0f4a4200df9e20f8e787e7cf1023dfb57d69ec2

--- MeshCNN-master/models/networks.py
+++ MeshCNN-master/models/networks.py
@@ -180,10 +180,9 @@ class MeshConvNet(nn.Module):
             features = torch.tensor([feature_values]).to(x.device)
             x = torch.cat((x, features), 1)
 
+        x = F.relu(self.fc1(x))
         if self.opt.dropout:
             x = self.d(x)
-
-        x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
 
