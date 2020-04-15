0b6f322366c5022cfc1737b144ab015e186c9774
Andy Wang <aw1912@ic.ac.uk>
2020-04-15 21:32:42 +0100

    updating gitignore to exclude notes
    Former-commit-id: a66841cf00e993e4d163cf80e1d17441873ebbf0

--- MeshCNN-master/scripts/brains/train_exp_red50_24gb.sh
+++ MeshCNN-master/scripts/brains/train_exp_red50_24gb.sh
@@ -1,1 +1,21 @@
-train_exp_red90_12gb_cls_TESTING_UPWEIGHT.sh
+#!/usr/bin/env bash
+
+## run the training
+python3 train.py \
+--dataroot datasets/brains_reg_red50 \
+--checkpoints_dir checkpoints/red50_24gb \
+--export_folder checkpoints/mesh_collapses \
+--name brains \
+--ninput_edges 48735 \
+--epoch_count 1 \
+--norm group \
+--num_aug 1 \
+--verbose_plot \
+--print_freq 10 \
+--seed 0 \
+--dataset_mode regression \
+--niter 5 \
+--niter_decay 100 \
+--batch_size 1 \
+--ncf 1024 1024 1536 2048 \
+--pool_res 10000 9000 8000 7000 \
