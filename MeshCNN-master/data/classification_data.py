69d948d31676dcb541bcb0f3f05d043493156a52
aw1912 <aw1912@ic.ac.uk>
2020-04-17 23:46:08 +0100

    adding debugs
    Former-commit-id: 5f41bf3b438f944e052490dbb045d698c62d1784

--- MeshCNN-master/data/classification_data.py
+++ MeshCNN-master/data/classification_data.py
@@ -57,6 +57,7 @@ class ClassificationData(BaseDataset):
                 continue
             for root, _, fnames in sorted(os.walk(d)):
                 for fname in sorted(fnames):
+                    print("DEBUG fname in meshretrieve ", fname)
                     if is_mesh_file(fname) and (root.count(phase)==1):
                         path = os.path.join(root, fname)
                         print("DEBUG meshretrieve path ", path)
