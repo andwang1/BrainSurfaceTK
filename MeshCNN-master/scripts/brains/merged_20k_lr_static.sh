5e40ed2833bb2b69959bf3870026790f303c8463
Andy Wang <aw1912@ic.ac.uk>
2020-04-18 15:31:14 +0100

    adding new scripts for all runs with static learning rate
    Former-commit-id: ee0166a2b30c1c4552439fa02c0d449f774e5220

---
+++ MeshCNN-master/scripts/brains/merged_20k_lr_static.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+## run the training
+python3 train.py \
+--dataroot datasets/brains_reg_merged_20k \
+--checkpoints_dir checkpoints/merged_20k_lr_static \
+--export_folder checkpoints/mesh_collapses \
+--name brains \
+--ninput_edges 48735 \
+--epoch_count 1 \
+--norm group \
+--num_aug 1 \
+--verbose_plot \
+--print_freq 10 \
+--seed 0 \
+--dataset_mode regression \
+--niter 100 \
+--niter_decay 0 \
+--batch_size 1 \
+--ncf 64 112 128 \
+--pool_res 3000 2750 2500 \
+--lr 0.0003 \
+--init_type kaiming \
+--num_groups 2 \
