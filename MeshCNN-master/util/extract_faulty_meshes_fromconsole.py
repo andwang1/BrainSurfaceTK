7ee5f52dbe446a733f89ea31e9e7ce21877c8344
Andy Wang <aw1912@ic.ac.uk>
2020-04-20 12:59:10 +0100

    adding new scripts for native and merged files
    Former-commit-id: fb17d59f03a6f21e4c65c6a962bac81d9be1959f

--- MeshCNN-master/util/extract_faulty_meshes_fromconsole.py
+++ MeshCNN-master/util/extract_faulty_meshes_fromconsole.py
@@ -1,4 +1,25 @@
-with open("lognative10k", "w") as f:
+# with open("lognative10k", "r") as f:
+#     log = [line.strip() for line in f.readlines()]
+#
+# faulty_meshes = []
+#
+# for i in range(len(log)):
+#     if log[i] =='HERE':
+#         x = log[i]
+#         element = i
+#         while x == 'HERE':
+#             element -= 1
+#             x = log[element]
+#
+#         faulty_meshes.append(x)
+#
+# print(faulty_meshes)
+# print(set(faulty_meshes))
+
+# print(len(set(faulty_meshes)))
+
+
+with open("logmerged10k.txt", "r") as f:
     log = [line.strip() for line in f.readlines()]
 
 faulty_meshes = []
@@ -16,23 +37,5 @@ for i in range(len(log)):
 print(faulty_meshes)
 print(set(faulty_meshes))
 
-with open("logmerged10k", "w") as f:
-    log = [line.strip() for line in f.readlines()]
-
-faulty_meshes = []
-
-for i in range(len(log)):
-    if log[i] =='HERE':
-        x = log[i]
-        element = i
-        while x == 'HERE':
-            element -= 1
-            x = log[element]
-
-        faulty_meshes.append(x)
-
-print(faulty_meshes)
-print(set(faulty_meshes))
-
-
+print(len(set(faulty_meshes)))
 
