50a917846e32727276ff9e00d7cfe5c82230f484
aw1912 <aw1912@ic.ac.uk>
2020-04-26 21:42:51 +0100

    updating scripts organisation
    Former-commit-id: 15f83dfb52c36dcc4c3a2226149e9f4714377ccf

--- MeshCNN-master/scripts/brains/temp/native_10k_lr_plateau_larger.sh
+++ MeshCNN-master/scripts/brains/temp/native_10k_lr_plateau_larger.sh
@@ -2,8 +2,8 @@
 
 ## run the training
 python3 train.py \
---dataroot datasets/brains_reg_native_5k \
---checkpoints_dir checkpoints/batch_batch_native_5k_lr_static \
+--dataroot datasets/brains_reg_native_10k \
+--checkpoints_dir checkpoints/native_10k_lr_plateau_larger \
 --export_folder checkpoints/mesh_collapses \
 --name brains \
 --epoch_count 1 \
@@ -13,11 +13,13 @@ python3 train.py \
 --print_freq 10 \
 --seed 0 \
 --dataset_mode regression \
---niter 200 \
---niter_decay 0 \
---batch_size 32 \
---ncf 64 112 128 \
---pool_res 3000 2750 2500 \
+--niter 1 \
+--niter_decay 100 \
+--batch_size 1 \
+--ncf 256 256 512 512 \
+--pool_res 3500 3000 2750 2500 \
 --lr 0.0003 \
 --init_type kaiming \
+--lr_policy plateau \
 --num_groups 2 \
+--min_lr 1e-5 \
--- MeshCNN-master/scripts/brains/view.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-python3 util/mesh_viewer.py \
---files \
-checkpoints/classification_models/brains/checkpoints/mesh_collapses/CC00170XX06_56100_0.obj \
-checkpoints/classification_models/brains/checkpoints/mesh_collapses/CC00170XX06_56100_1.obj \
-checkpoints/classification_models/brains/checkpoints/mesh_collapses/CC00170XX06_56100_2.obj \
-checkpoints/classification_models/brains/checkpoints/mesh_collapses/CC00170XX06_56100_3.obj \
-checkpoints/classification_models/brains/checkpoints/mesh_collapses/CC00170XX06_56100_4.obj
