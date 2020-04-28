9890274df94b631965b689730fac6df599417e4e
aw1912 <aw1912@ic.ac.uk>
2020-04-28 22:49:33 +0100

    adding new scripts
    Former-commit-id: 6341a709d0688aeef60105eef8e26a5ab36a1e81

--- MeshCNN-master/scripts/brains/cls_preterm_merged_10k_lr_static.sh
+++ MeshCNN-master/scripts/brains/cls_preterm_merged_10k_lr_static.sh
@@ -2,8 +2,8 @@
 
 ## run the training
 python3 train.py \
---dataroot datasets/brains_reg_merged_10k_LR \
---checkpoints_dir checkpoints/merged_10k_leftright_lr_plateau \
+--dataroot datasets/brains_cls_binary_preterm_merged_10k \
+--checkpoints_dir checkpoints/cls_preterm_merged_10k_lr_static \
 --export_folder checkpoints/mesh_collapses \
 --name brains \
 --epoch_count 1 \
@@ -12,14 +12,12 @@ python3 train.py \
 --verbose_plot \
 --print_freq 10 \
 --seed 0 \
---dataset_mode regression \
---niter 1 \
---niter_decay 200 \
+--dataset_mode binary_class \
+--niter 200 \
+--niter_decay 0 \
 --batch_size 1 \
 --ncf 64 112 128 \
 --pool_res 3000 2750 2500 \
 --lr 0.0003 \
 --init_type kaiming \
---lr_policy plateau \
 --num_groups 2 \
---min_lr 1e-5 \
