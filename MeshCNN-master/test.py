001e0c2d9e8977fa6faa0e8afff2e720d38d6c02
Andy Wang <aw1912@ic.ac.uk>
2020-04-29 11:25:11 +0100

    fixing bug in val runs when the model is continued
    Former-commit-id: 8b6b75f4e00a6ff0fa3c224e83b716b62d829c8d

--- MeshCNN-master/test.py
+++ MeshCNN-master/test.py
@@ -9,8 +9,13 @@ def run_test(epoch=-1, is_val=True):
     opt = TestOptions().parse()
     # No shuffling for test set
     opt.serial_batches = True
+
+    opt.which_epoch = epoch
+
     # If testing outside of training, want the epoch number to be correct so the files are created correctly
     if epoch == -1:
+        if opt.verbose:
+            print(f"DEBUG: Epoch given -1, setting equal to which_epoch {opt.which_epoch}")
         epoch = opt.which_epoch
     # Set batch_size to 1
     opt.batch_size = 1
