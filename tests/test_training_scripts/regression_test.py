2952e8f1ec4f19197eb7960ae0d347d205766f84
aw1912 <aw1912@ic.ac.uk>
2020-05-08 16:01:34 +0100

    removing tests folder from main repo level
    Former-commit-id: b2a5133b739e9cf643bfe6eff8f51a60da296c04

--- tests/test_training_scripts/regression_test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-print("starting test")
-try:
-    subprocess.call("/vol/biomedic2/aa16914/shared/MScAI_brain_surface/rhys/deepl_brain_surfaces/MeshCNN-master/scripts/brains/regression_test.sh")
-    print("Regression Test Passed")
-except:
-    print("Regression Test Failed")
-
