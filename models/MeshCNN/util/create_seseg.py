e23ef6ca0b184a6676c05f1615db3983050a74e8
aw1912 <aw1912@ic.ac.uk>
2020-05-09 23:24:48 +0100

    adding utility scripts
    Former-commit-id: 48195b2a8ec84d65b1cdae96597140b5153232ed

---
+++ models/MeshCNN/util/create_seseg.py
@@ -0,0 +1,16 @@
+def write_seseg(eseg_path, seseg_path, patient_id, ses_id):
+    eseg_file = eseg_path + patient_id +"_"+ses_id+".eseg"
+    seseg_file = seseg_path + patient_id +"_"+ses_id+".seseg"
+    with open(eseg_file) as f:
+        eseg = f.read().splitlines()
+
+    labels = range(38)
+
+    with open(seseg_file, 'w') as f:
+        for label in eseg:
+            row = [0 if l is not int(label) else 1 for l in labels]
+            f.write(str(row).strip("[]").replace(",", ""))
+            f.write("\n")
+
+if __name__ == "__main__":
+
