b110b236130c009fef080c9ebe3e741fadd03fe9
Ward, Francis R <francis.ward19@imperial.ac.uk>
2020-05-10 14:25:19 +0100

    Add view mesh with pyvista script
    Former-commit-id: 13cafc8a68ea876ba331d92f36935eb1650b3f87

---
+++ models/MeshCNN/util/view_mesh_pyvista.py
@@ -0,0 +1,14 @@
+import pyvista as pv
+import sys
+ 
+file_path = sys.arg[1]
+mesh = pv.read(file_path)
+surf_faces = pv.PolyData(mesh.points, mesh.faces)
+plotter = pv.Plotter()
+surf_points = pv.PolyData(mesh.points[[4185, 4181, 4179]])
+plotter.add_mesh(surf_points, show_edges=True, render_points_as_spheres=True, point_size=10, color='red')
+plotter.add_mesh(surf_faces)
+
+if __name__ == "__main__":
+    plotter.show()
+              
