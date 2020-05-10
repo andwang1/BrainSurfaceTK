5298ff240f24c20ebbe8ab2c0bca66d7b35c0e22
Ward, Francis R <francis.ward19@imperial.ac.uk>
2020-05-10 14:13:16 +0100

    Update vtk_to_obj_convert.py to accept command line args
    Former-commit-id: 725f976872212409ff237070e009b91b7bb6f664

--- models/MeshCNN/util/vtk_to_obj_convert.py
+++ models/MeshCNN/util/vtk_to_obj_convert.py
@@ -2,47 +2,59 @@ import os
 import numpy as np
 import pyvista as pv
 from read_meta import read_meta
+
 from get_edge_features import write_eseg, from_scratch, write_seseg, save_features
 
-__author__ = "Francis Rhys Ward"
-__license__ = "MIT"
+#pyvista docs: https://docs.pyvista.org/plotting/plotting.html#pyvista.BasePlotter.add_mesh
 
-meta_data_path = "combined.tsv"
-vtk_path = "datasets/reducedto_10k/pial/vtk/"
-path = "datasets/seg_10k_left/"
-obj_path = path + "obj/"
-seg_path = path + "seg/"
-sseg_path = path + "sseg/"
-feat_path = path + "local_features/"
+if __name__ == '__main__':
 
-extension = "_left_pial_10k.vtk"
-meta_data = read_meta(meta_data_path)
-patient_names = []
-ses_ids = []
 
-for idx, ids in enumerate(meta_data):
-    patient_id, ses_id = ids[0], ids[1]
-    print(patient_id, ses_id)
-
-    p = pv.Plotter(off_screen=True)
-
+    import sys
+    meta_data_path = sys.argv[1]
+    vtk_path = sys.argv[2]
+    path = sys.argv[3]
     try:
-        meshv = pv.read(vtk_path + "sub-" + patient_id + "_ses-" + ses_id + extension)
+        seg = sys.argv[4] == "seg"
     except:
-        print("failed")
-        continue
-    p.add_mesh(meshv, show_edges=True)
+        seg = False
+    #meta_data_path = "/vol/biomedic/users/aa16914/shared/data/dhcp_neonatal_brain/combined.tsv"
+    #vtk_path = "/vol/biomedic/users/aa16914/shared/data/dhcp_neonatal_brain/surface_native_04152020/hemispheres/reducedto_10k/pial/vtk/"
+    #path = "/vol/biomedic2/aa16914/shared/MScAI_brain_surface/rhys/deepl_brain_surfaces/MeshCNN-master/datasets/seg_10k_left/"
+    obj_path = path+"obj/"
+    seg_path = path+"seg/"
+    sseg_path = path+"sseg/"
+    feat_path = path+"local_features/"
 
-    p.export_obj(obj_path + patient_id + "_" + ses_id)
+    extension = "_left_pial_10k.vtk"
+    meta_data = read_meta(meta_data_path)
+    patient_names = []
+    ses_ids = []
 
-    feature_arrays = {'drawem': 0, 'corr_thickness': 1, 'myelin_map': 2, 'curvature': 3, 'sulc': 4}
-    vert_features = {feature: meshv.get_array(feature_arrays[feature]) for feature in feature_arrays.keys()}
 
-    mesh_data = from_scratch(file=obj_path + patient_id + "_" + ses_id + ".obj", opt=None)
+    for idx, ids in enumerate(meta_data):
+        patient_id, ses_id = ids[0], ids[1]
+        print(patient_id, ses_id)
 
-    write_eseg(mesh_data, vtk_path + "sub-" + patient_id + "_ses-" + ses_id + extension, seg_path, patient_id, ses_id)
+        p = pv.Plotter(off_screen=True)
 
-    write_seseg(seg_path, sseg_path, patient_id, ses_id)
+        try:
+            meshv = pv.read(vtk_path+"sub-"+patient_id+"_ses-"+ses_id+extension)
+        except:
+            print("failed")
+            continue
+        p.add_mesh(meshv, show_edges=True)
 
-    save_features(mesh_data, vtk_path + "sub-" + patient_id + "_ses-" + ses_id + extension, feat_path, patient_id,
-                  ses_id)
+        p.export_obj(obj_path+patient_id+"_"+ses_id)
+
+        if seg:
+            feature_arrays = {'drawem': 0, 'corr_thickness': 1, 'myelin_map': 2, 'curvature': 3, 'sulc': 4}
+            vert_features = {feature:meshv.get_array(feature_arrays[feature]) for feature in feature_arrays.keys()}
+
+            mesh_data = from_scratch(file=obj_path+ patient_id+"_"+ses_id+".obj", opt=None)
+
+            write_eseg(mesh_data, vtk_path+"sub-"+patient_id+"_ses-"+  ses_id+extension, seg_path, patient_id, ses_id)
+
+            write_seseg(seg_path, sseg_path, patient_id, ses_id)
+
+            save_features(mesh_data, vtk_path+"sub-"+patient_id+"_ses-"+ses_id+extension, feat_path, patient_id, ses_id)
