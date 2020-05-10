53bea1549972d12529000dc3910baa2dff9ea264
Ward, Francis R <francis.ward19@imperial.ac.uk>
2020-05-10 12:24:35 +0100

    Update test_get_feature.py
    Former-commit-id: 4d2b8011974764235fef7ab3bfaa815510df7762

--- models/MeshCNN/tests/test_functions/test_get_feature.py
+++ models/MeshCNN/tests/test_functions/test_get_feature.py
@@ -1,29 +1,27 @@
+import unittest
 import sys
-from data.get_feature_dict import get_feature_dict
+sys.path.insert(1, "/vol/biomedic2/aa16914/shared/MScAI_brain_surface/rhys/deepl_brain_surfaces/MeshCNN-master/data")
+from get_feature_dict import get_feature_dict
 
-__author__ = "Francis Rhys Ward"
-__license__ = "MIT"
+SUBJECT = "CC01006XX08_38531"
 
-CC01006XX08_38531_features = ["Male", 28.714285714285715, 1.03, 30.571428571429, 1]
-features = ["gender", "birth_age", "birth_weight", "scan_age", "scan_number"]
+class Test_get_feature(unittest.TestCase):
 
-def test():
-    test_pass = True
-    for i, feature in enumerate(features):
-        actual_val = CC01006XX08_38531_features[i]
-        try:
-            test_val = get_feature_dict(feature)["CC01006XX08_38531"]
-        except:
-            test_pass = False
-            print(f"Failed to get function value for {feature}.")
-            continue
-        try:
-            assert test_val == actual_val
-        except:
-            test_pass = False
-            print(f"Failed to get feature {feature}. Expected {actual_val} of type {type(actual_val)}, got {test_val} of type {type(test_val)}")
-    return test_pass
+    def test_gender(self):
+        self.assertEqual(get_feature_dict("gender")[SUBJECT], "Male", "Should be Male")
 
-if __name__ == "__main__":
-    test_passed = test()
-    print("get_feature_dict test passed = ", test_passed)
+    def test_birth_age(self):
+        self.assertEqual(round(get_feature_dict("birth_age")[SUBJECT], 3), 28.714, "Should be 28.714")
+
+    def test_birth_weight(self):
+        self.assertEqual(get_feature_dict("birth_weight")[SUBJECT], 1.03, "Should be 1.03")
+
+    def test_scan_age(self):
+        self.assertEqual(round(get_feature_dict("scan_age")[SUBJECT], 3), 30.571, "Should be 1.03")
+
+    def test_scan_number(self):
+        self.assertEqual(get_feature_dict("scan_number")[SUBJECT], 1, "Should be 1")
+
+
+if __name__ == '__main__':
+  unittest.main()
