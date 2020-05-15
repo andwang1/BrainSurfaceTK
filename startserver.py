03b1730e3525d7abc0a790b9fbb0bedc252b9bb2
Cemlyn N Waters <cnw119@ic.ac.uk>
2020-05-15 15:23:31 +0100

    Merging new model implementation with GUI

--- startserver.py
+++ startserver.py
@@ -1,26 +1,29 @@
 import os
 import sys
+import time
 
 SETTINGS = "GUI/BasicSite/settings.py"
 MANAGE_PATH = "GUI/manage.py"
-MEDIA_PATH = "/vol/biomedic2/aa16914/shared/MScAI_brain_surface/media"
+MEDIA_ROOT = "/vol/biomedic2/aa16914/shared/MScAI_brain_surface/media"
 
 
-def set_debug_to(boolv=True):
-    if boolv:
+def modify_settings(set_debug_status_to=True, media_path=""):
+    if set_debug_status_to:
         cboolv = False
     else:
         cboolv = True
-    boolv = str(boolv).capitalize()
+    set_debug_status_to = str(set_debug_status_to).capitalize()
     cboolv = str(cboolv).capitalize()
     info = dict()
     with open(SETTINGS, 'r') as file:
         filedata = file.readlines()
     for i, line in enumerate(filedata):
         if "DEBUG" in line:
-            filedata[i] = line.replace(cboolv, boolv)
-        if "MEDIA_URL" in line:
+            filedata[i] = line.replace(cboolv, set_debug_status_to)
+        if "MEDIA_URL" in line and line.strip(" ")[0] != "#":
             info["MEDIA_URL"] = line.split(" ")[-1].split('"')[-2][:-1]
+        if "MEDIA_ROOT" in line and line.strip(" ")[0] != "#":
+            filedata[i] = 'MEDIA_ROOT = ' + '\"' + str(media_path) + '\"' + "\n"
     with open(SETTINGS, 'w') as file:
         file.writelines(filedata)
     return info
@@ -33,11 +36,19 @@ if __name__ == "__main__":
     os.system(" ".join(["python", MANAGE_PATH, "makemigrations", "--noinput"]))
     os.system(" ".join(["python", MANAGE_PATH, "migrate"]))
 
+    if "imperial" not in args:
+        MEDIA_ROOT = "GUI/media"
+
     if "prod" in args:
-        info = set_debug_to(False)
-        print("--url-alias", info["MEDIA_URL"], MEDIA_PATH)
+        info = modify_settings(False, MEDIA_ROOT)
+        print(MEDIA_ROOT)
+        print(os.getcwd())
+        time.time()
+        print("--url-alias", info["MEDIA_URL"], MEDIA_ROOT)
         os.system(r" ".join(["python", MANAGE_PATH, "collectstatic", "--noinput"]))
-        os.system(r" ".join(["python", MANAGE_PATH, "runmodwsgi", "--url-alias", info["MEDIA_URL"], MEDIA_PATH, "--limit-request-body", "104857600", "--request-timeout 120"]))
+        os.system(r" ".join(
+            ["python", MANAGE_PATH, "runmodwsgi", "--url-alias", info["MEDIA_URL"], MEDIA_ROOT, "--limit-request-body",
+             "104857600", "--request-timeout 120"]))
     else:
-        set_debug_to(True)
+        modify_settings(True, MEDIA_ROOT)
         os.system(" ".join(["python", MANAGE_PATH, "runserver"]))
