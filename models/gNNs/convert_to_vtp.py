d0a7e29c2720daea9baffa7e5f0d2472c9b4083a
Cemlyn N Waters <cnw119@ic.ac.uk>
2020-06-01 12:28:52 +0100

    Converter file possibly

--- models/gNNs/convert_to_vtp.py
+++ models/gNNs/convert_to_vtp.py
@@ -4,6 +4,9 @@ from concurrent.futures import ThreadPoolExecutor
 import pyvista as pv
 from tqdm import tqdm
 
+"""
+TODO: Keep features as well :)
+"""
 
 def convert_to_vtp(path, new_path):
     if not os.path.exists(new_path):
@@ -35,8 +38,8 @@ def convert_file_to_vtp(filepath, new_path):
 
 
 if __name__ == "__main__":
-    # path = "/mnt/UHDD/Programming/Projects/DeepLearningOnBrains/models/gNNs/data"
+    # path = "/mnt/UHDD/Programming/Projects/DeepLearningOnBrains/models/gNNs/tmp_data/vtks"
     path = "/vol/biomedic/users/aa16914/shared/data/dhcp_neonatal_brain/surface_native/reduced_20k/white"
-    # new_path = "/mnt/UHDD/Programming/Projects/DeepLearningOnBrains/models/gNNs/tmp_data"
+    # new_path = "/mnt/UHDD/Programming/Projects/DeepLearningOnBrains/models/gNNs/tmp_data/vtps"
     new_path = "/vol/bitbucket/cnw119/data/vtps"
     convert_to_vtp(path, new_path)
