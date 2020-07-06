8943e720c3e97ee94908bc2c4fdf1fb729a8851d
Cemlyn N Waters <cnw119@ic.ac.uk>
2020-07-06 17:07:32 +0100

    ready for slurm

---
+++ models/gNNs/L-GCN-20k.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cnw119
+whoami
+export PATH=/vol/bitbucket/${USER}/miniconda3/bin/:$PATH
+source activate
+conda activate vortexAI
+# source /vol/cuda/10.0.130/setup.sh
+TERM=vt100 # or TERM=xterm
+/usr/bin/nvidia-smi
+uptime
+cd /vol/bitbucket/${USER}/neodeepbrain || exit
+
+python -u -m models.gNNs.basicgcntrain left 20k False some --batch_size 32 --save_path ../tmp2 --results ./results2
