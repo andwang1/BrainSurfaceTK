4015b2e7d9b2adcf5265752898c8cb7cd2f03158
Cemlyn N Waters <cnw119@ic.ac.uk>
2020-07-06 17:38:05 +0100

    fixed scripts

--- models/gNNs/Merged-GCN-20k.sh
+++ models/gNNs/Merged-GCN-20k.sh
@@ -3,13 +3,12 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=cnw119
 whoami
-export PATH=/vol/bitbucket/cnw119/miniconda3/bin/:$PATH
-source activate
+source /vol/bitbucket/cnw119/miniconda3/etc/profile.d/conda.sh
 conda activate vortexAI
 # source /vol/cuda/10.0.130/setup.sh
 TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
 uptime
-cd /vol/bitbucket/${USER}/neodeepbrain || exit
+cd /vol/bitbucket/cnw119/neodeepbrain || exit
 
 python -u -m models.gNNs.basicgcntrain merged 20k False some --batch_size 32 --save_path ../tmp2 --results ./results2
